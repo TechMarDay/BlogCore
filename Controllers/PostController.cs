50ceb580579f5d0b6b7d18ffa89af3e877888b8b
Long Nguyen <longdhn.lk@gmail.com>
2020-09-15 20:15:40 +0700

    filter category

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using BlogCore.Data;
 using BlogCore.Models;
+using BlogCore.Extension;
 
 namespace BlogCore.Controllers
 {
@@ -27,6 +28,7 @@ namespace BlogCore.Controllers
         //}
 
         // GET: Post/Details/5
+        [HttpGet("bai-viet/{id}")]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -62,6 +64,36 @@ namespace BlogCore.Controllers
             }
 
             return View(postModel);
+        }
+
+        [HttpGet("danh-muc/{id}")]
+        public async Task<IActionResult> PostByCategory(int id, int? currentPage)
+        {
+            if (currentPage == null || currentPage == 0)
+                currentPage = 1;
+
+            var postsQuery = from p in _context.Posts
+                             join c in _context.Categories
+                                  on p.CategoryId equals c.Id
+                             select new PostModel
+                             {
+                                 Id = p.Id,
+                                 Title = p.Title,
+                                 Content = p.Content,
+                                 CategoryId = p.CategoryId,
+                                 Image = p.Image,
+                                 LastModificationTime = p.LastModificationTime == null
+                                ? p.CreationTime : (DateTime)p.LastModificationTime,
+                                 Category = new CategoryModel
+                                 {
+                                     Id = c.Id,
+                                     Name = c.Name
+                                 }
+                             };
+
+            var posts = await postsQuery.Where(x => x.CategoryId == id)
+                .ToPagedListAsync<PostModel>((int)currentPage, 6);
+            return View(posts);
         }
 
         //// GET: Post/Create
